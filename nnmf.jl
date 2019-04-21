ae1ee9d0bf4d4f0022e1e12e46679162ac82a92d
server <none>
2019-04-21 14:15:09 -0500

    fixed type mismatch between epsilon and GPU array

--- nnmf.jl
+++ nnmf.jl
@@ -6,14 +6,15 @@ function nnmf(fileName,num_components,max_iter)
     num_frames = length(Vbig);
     W_h = rand(num_freq_bins,num_components,num_frames);
     H_h = rand(num_components,frame_size,num_frames);
+    epsilon1 = cu(1e-9*ones(num_components,frame_size));
+    epsilon2 = cu(1e-9*ones(num_freq_bins,num_components));
     for i_frame=1:length(Vbig)
         V = cu(Vbig[i_frame]);
-        #remember size performance issue
         W = cu(W_h[:,:,i_frame]);
         H = cu(H_h[:,:,i_frame]);
         for i_iter = 1:max_iter
-            H = H .* (W'*V) ./ (W'*W*H .+ 1e-9)
-            W = W .* (V*H') ./ (W*H*H' .+ 1e-9)
+            H = H .* (W'*V) ./ (W'*W*H .+ epsilon1)
+            W = W .* (V*H') ./ (W*H*H' .+ epsilon2)
         end
         W_h[:,:,i_frame] = collect(W);
         H_h[:,:,i_frame] = collect(H);
